80964aabd749ad5542fbfe5e8f96d303510d43ff
Josh Wright <josh.wright@edapp.com>
2022-09-12 13:53:42 +1000

    tidy up

--- kube-deploy.sh
+++ kube-deploy.sh
@@ -25,45 +25,67 @@ Commands:
 
 init() {
   cli_log "Deploying Kubernetes Certificate Manager"
-  kubectl apply -f https://github.com/cert-manager/cert-manager/releases/latest/download/cert-manager.yaml
+  helm upgrade \
+    --install cert-manager cert-manager \
+    --repo https://charts.jetstack.io \
+    --namespace cert-manager --create-namespace \
+    --set installCRDs=true \
+    --wait --timeout=120s
   cmctl check api --wait=2m
   
   cli_log "Deploying Ingress Controller"
-  helm upgrade --install ingress-nginx ingress-nginx \
+  helm upgrade \
+    --install ingress-nginx ingress-nginx \
     --repo https://kubernetes.github.io/ingress-nginx \
-    --namespace ingress-nginx --create-namespace
-  
-  kubectl wait --namespace ingress-nginx \
-    --for=condition=ready pod \
-    --selector=app.kubernetes.io/component=controller \
-    --timeout=120s
+    --namespace ingress-nginx --create-namespace \
+    --wait --timeout=120s
+      
+  cli_log "Deploying Dapr"
+  helm upgrade \
+    --install dapr dapr \
+    --repo https://dapr.github.io/helm-charts/ \
+    --namespace dapr-system --create-namespace \
+    --set global.ha.enabled=true \
+    --wait --timeout=120s
+    
   
   cli_log "Deploying OpenTelemetry Operator"
-  kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
-  kubectl wait --for=condition=available --timeout=120s deployment/opentelemetry-operator-controller-manager -n opentelemetry-operator-system
+  helm upgrade --install opentelemetry-operator opentelemetry-operator \
+    --repo https://open-telemetry.github.io/opentelemetry-helm-charts \
+    --namespace opentelemetry-operator-system --create-namespace \
+    --wait --timeout=120s
   
   cli_log "Initialised."
 }
 
 term() {
   # Create OpenTelemetry Operator
-  cli_log "Terminating OpenTelemetry Operator"
-  kubectl delete -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
-  kubectl wait --all --namespace opentelemetry-operator-system --timeout=120s --for=delete "pod"
+  cli_log "Removing OpenTelemetry Operator"
+  helm uninstall opentelemetry-operator \
+      --namespace opentelemetry-operator-system \
+      --wait --timeout=120s
+
+  cli_log "Removing Dapr"
+  helm uninstall dapr \
+    --namespace dapr-system \
+    --wait --timeout=120s
   
-  cli_log "Terminating Ingress Controller"
-  kubectl delete -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.3.1/deploy/static/provider/cloud/deploy.yaml
-  kubectl wait -n ingress-nginx --timeout=120s --all --for=delete "pod"
-    
+  cli_log "Removing Ingress Controller"
+  helm uninstall ingress-nginx \
+    --namespace ingress-nginx \
+    --wait --timeout=120s
+  
   # Create Kubernetes Certificate Manager
   cli_log "Terminating Kubernetes Certificate Manager"
-  kubectl delete -f https://github.com/cert-manager/cert-manager/releases/latest/download/cert-manager.yaml
-  kubectl wait --all --namespace cert-manager --timeout=120s --for=delete "pod"
+  helm uninstall cert-manager \
+    --namespace cert-manager \
+    --wait --timeout=120s
   
   cli_log "Terminated."
 }
 
 up() {
+  local label=$(date +%s)
   # Create Namespace
   cli_log "Creating Namespace"
   kubectl apply -f Infra/namespace.yaml
@@ -78,6 +100,12 @@ up() {
   kubectl wait --for=condition=available --timeout=120s deployment/otel-collector -n ${NAMESPACE}
   
   # Create User Groups Demo Service
+  cli_log "Building User Groups Api"
+  docker build  \
+    --tag "ed-demo/dapr-usergroups-api:latest" \
+    --tag "ed-demo/dapr-usergroups-api:$label" \
+    --file Services/UserGroups.Api/Dockerfile \
+    .
   cli_log "Creating User Groups Api"
   kubectl apply -f Services/UserGroups.Api/UserGroups.Api.yaml  -n ${NAMESPACE}
 }
